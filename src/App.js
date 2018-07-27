6774f7e0cbd0f9c01fe0b21eab5bd0bb74016250
Brandon Rose <[brandonrose805@gmail.com]>
2018-07-26 23:21:12 -0700

    display RoomList component and add Firebase to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import RoomList from './components/RoomList';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCoER4b0pGoLC8dgZZSAmvaIJ4uVjJdUYY",
+  authDomain: "chatterbox-react.firebaseapp.com",
+  databaseURL: "https://chatterbox-react.firebaseio.com",
+  projectId: "chatterbox-react",
+  storageBucket: "chatterbox-react.appspot.com",
+  messagingSenderId: "722779105914"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="sidebar">
+          <h3>Chatterbox</h3>
+          <RoomList firebase={firebase} />
+        </div>
+        <div className="chat-container">
+        </div>
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import '../style/RoomList.css';
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: [],
+      newRoomName: '',
+      display:"none"
+    };
+    this.hideForm = this.hideForm.bind(this);
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) });
+    });
+  }
+
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value })
+  }
+
+  handleSumbmit(e) {
+    e.preventDefault();
+    if (!this.state.newRoomName) { return }
+    this.setState({ newRoomName: '' });
+    this.hideForm();
+  }
+
+  createRoom() {
+    this.roomsRef.push({
+      name: this.state.newRoomName
+    })
+  }
+
+  hideForm() {
+    const newDisplay = this.state.display === 'none' ? 'block' : 'none';
+    this.setState({ display: newDisplay });
+  }
+
+
+  render() {
+    return(
+      <div className="room-list">
+        <ul>
+          {
+            this.state.rooms.map(room => {
+              return <li key={room.key}>{room.name}</li>
+            })
+          }
+        </ul>
+        <button classname="new-room-button" onClick={this.hideForm}>New Room</button>
+        <form style={{display: this.state.display}} onSubmit={ (e) => this.handleSumbmit(e) }>
+          <input type="text" value={ this.state.newRoomName } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" onClick={ () => this.createRoom() } />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default RoomList;
